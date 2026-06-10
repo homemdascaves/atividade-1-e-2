44a44e7a038fee2060a50e98d05a2c882c822326
homemdascaves <gustavosalgado1404@gmail.com>
2026-06-10 18:00:23 -0300

    Add files via upload

---
+++ lista 1/exercicio11.js
@@ -0,0 +1,9 @@
+let anosDeVida
+let mesesDeVida
+
+
+anosDeVida = parseFloat(prompt("Informe quantos anos o aluno possui"))
+
+mesesDeVida = anosDeVida * 12
+
+alert("O aluno tem: " + mesesDeVida + "meses de vida")

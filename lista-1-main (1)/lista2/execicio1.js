9774af16f095ebb96261270bb45a3f059c0c6eda
homemdascaves <gustavosalgado1404@gmail.com>
2026-06-14 14:19:15 -0300

    Delete lista-1-main (1)/lista2 directory

--- lista-1-main (1)/lista2/execicio1.js
+++
@@ -1,6 +0,0 @@
-let idadeDaPessoa
-
-idadeDaPessoa = prompt("Informe a idade da pessoa")
-
-if(idadeDaPessoa >= 18) {alert("Maior de idade")}
-if(idadeDaPessoa < 18) {alert("Menor de idade")}
--- lista-1-main (1)/lista2/execicio2.js
+++
@@ -1,6 +0,0 @@
-let numeroSolicitado
-
-numeroSolicitado = prompt("Informe o número")
-
-if(numeroSolicitado >= 0) {alert("O número é positivo")}
-if(numeroSolicitado < 0) {alert("O néume é negativo")}

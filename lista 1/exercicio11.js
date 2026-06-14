397d8e8b9c5796da849062f54e1e415eb0b5f420
homemdascaves <gustavosalgado1404@gmail.com>
2026-06-14 14:19:04 -0300

    Delete lista 1 directory

--- lista 1/exercicio11.js
+++
@@ -1,9 +0,0 @@
-let anosDeVida
-let mesesDeVida
-
-
-anosDeVida = parseFloat(prompt("Informe quantos anos o aluno possui"))
-
-mesesDeVida = anosDeVida * 12
-
-alert("O aluno tem: " + mesesDeVida + "meses de vida")

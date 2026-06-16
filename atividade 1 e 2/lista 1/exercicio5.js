c745298554bba03ff6fb10fc18058243707c2f24
homemdascaves <gustavosalgado1404@gmail.com>
2026-06-16 16:30:41 -0300

    Delete atividade 1 e 2 directory

--- atividade 1 e 2/lista 1/exercicio5.js
+++
@@ -1,8 +0,0 @@
-let areaDaParede
-let quantidadeDeLitros
-
-areaDaParede = parseFloat(prompt("Informe a area da parede"))
-
-quantidadeDeLitros = areaDaParede / 5
-
-alert("A quantidade de tinta gasta é: " + quantidadeDeLitros)
